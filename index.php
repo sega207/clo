bafb90578e5f284c37dd3fda748c74a35ead43d3
sega207 <129535907+sega207@users.noreply.github.com>
2023-04-01 12:37:31 +0500

    1

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Site Metas -->
+     
+    <title>Início</title>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- ALL VERSION CSS -->
+    <link rel="stylesheet" href="css/versions.css" />
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css" />
+  </head>
+  <body class="host_version">
+    <!-- LOADER -->
+    <div id="preloader">
+      <div class="loader-container">
+        <div class="progress-br float shadow">
+          <div class="progress__item"></div>
+        </div>
+      </div>
+    </div>
+    <!-- END LOADER -->
+
+    <!-- Start header -->
+    <header class="top-navbar">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white" href="index.php"> O truque </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbars-host"
+            aria-controls="navbars-rs-food"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbars-host">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Início</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="blog.php">Artigo</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contactos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="policy.php">Política de privacidade</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="terms.php">Termos e condições</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- End header -->
+
+    <div
+      id="carouselExampleControls"
+      class="carousel slide bs-slider box-slider"
+      data-ride="carousel"
+      data-pause="hover"
+      data-interval="false"
+    >
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+          <div
+            id="home"
+            class="first-section"
+            style="background-image: url('./assets/images/will-porada-zlli7ptd8fg-unsplash.jpg')"
+          >
+            <div class="dtab">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12 col-sm-12 text-center">
+                    <div class="big-tagline">
+                      <h2><strong>Popular em</strong></h2>
+                    </div>
+                  </div>
+                </div>
+                <!-- end row -->
+              </div>
+              <!-- end container -->
+            </div>
+          </div>
+          <!-- end section -->
+        </div>
+      </div>
+    </div>
+
+    <div id="overviews" class="section wb">
+      <div class="container">
+        <div class="section-title row text-center">
+          <div class="col-md-8 offset-md-2">
+            <h3>Artigo</h3>
+          </div>
+        </div>
+        <!-- end title -->
+        
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="post-media wow fadeIn">
+              <img src="./assets/images/will-porada-zlli7ptd8fg-unsplash.jpg" alt="" class="img-fluid img-rounded" />
+            </div>
+            <!-- end media -->
+          </div>
+          <!-- end col -->
+
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="message-box">
+              <h2>Top 5 jogos de tabuleiro: Excelente resumo de estr...</h2>
+              <p><strong></strong> Abstrato estratégias são um dos gêneros mais populares em jogos de tabuleiro agora. Estes jogos são associados com uma clara de ...</p>
+              <a href="GAxe7pVgvs6DUkFLfWpgkTRfil.php" class="hover-btn-new orange"
+                ><span>Leia mais</span></a
+              >
+            </div>
+            <!-- end messagebox -->
+          </div>
+          <!-- end col -->
+        </div>
+        
+
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="post-media wow fadeIn">
+              <img src="./assets/images/florian-olivo-mf23rf8xary-unsplash.jpg" alt="" class="img-fluid img-rounded" />
+            </div>
+            <!-- end media -->
+          </div>
+          <!-- end col -->
+
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="message-box">
+              <h2>Top 5 jogos de tabuleiro baseado em jogos de compu...</h2>
+              <p><strong></strong> Bom dia a todos!!! Nós decidimos tentar novamente para publicar o top 5 desta vez com foco em jogos de tabuleiro que foram criados,...</p>
+              <a href="xC6mrvwzfl08bNYCO2GcMmXF.php" class="hover-btn-new orange"
+                ><span>Leia mais</span></a
+              >
+            </div>
+            <!-- end messagebox -->
+          </div>
+          <!-- end col -->
+        </div>
+        
+
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="post-media wow fadeIn">
+              <img src="./assets/images/alex-haney-xwkryosf8_c-unsplash.jpg" alt="" class="img-fluid img-rounded" />
+            </div>
+            <!-- end media -->
+          </div>
+          <!-- end col -->
+
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="message-box">
+              <h2>Resumindo o top 5 jogos de tabuleiro: 5 Grandes jo...</h2>
+              <p><strong></strong> Não há muito tempo, escrevemos um artigo sobre 5 grandes jogos de tabuleiro cerca de gladiadores. Hoje vamos tentar novamente para...</p>
+              <a href="u1cAHuH1sEPTh9bVQMcNHpvBO.php" class="hover-btn-new orange"
+                ><span>Leia mais</span></a
+              >
+            </div>
+            <!-- end messagebox -->
+          </div>
+          <!-- end col -->
+        </div>
+        
+
+        <div class="row align-items-center">
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="post-media wow fadeIn">
+              <img src="./assets/images/andrik-langfield-s0_ep9melfy-unsplash.jpg" alt="" class="img-fluid img-rounded" />
+            </div>
+            <!-- end media -->
+          </div>
+          <!-- end col -->
+
+          <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="message-box">
+              <h2>Top 5 jogos de tabuleiro: Excelente resumo de estr...</h2>
+              <p><strong></strong> Resumo os jogos de estratégia são um dos gêneros mais populares de jogos de tabuleiro modernos. Estes jogos são baseados na idé...</p>
+              <a href="dee6e2a602cf18428ce08603061f2077.php" class="hover-btn-new orange"
+                ><span>Leia mais</span></a
+              >
+            </div>
+            <!-- end messagebox -->
+          </div>
+          <!-- end col -->
+        </div>
+        
+
+        <!-- end row -->
+      </div>
+      <!-- end container -->
+    </div>
+    <!-- end section -->
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-xs-12 text-center">
+            <div class="widget clearfix">
+              <div class="widget-title">
+                <h3>Contactos</h3>
+              </div>
+              <p>Largo de Lourenço, 38 1801-356 Entroncamento</p>
+              <p>+9712846740184</p>
+              <p>ucosta@neto.com</p>
+            </div>
+            <!-- end clearfix -->
+          </div>
+        </div>
+        <!-- end row -->
+      </div>
+      <!-- end container -->
+    </footer>
+    <!-- end footer -->
+
+    <div class="copyrights">
+      <div class="container">
+        <div class="footer-distributed">
+          <div class="footer-center">
+            <p class="footer-company-name">
+              ©
+              <script>
+                document.write(new Date().getFullYear());
+              </script>
+              Copyright
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- end container -->
+    </div>
+    <!-- end copyrights -->
+
+    <a href="#" id="scroll-to-top" class="dmtop global-radius"><i class="fa fa-angle-up"></i></a>
+
+    <div class="cookie-banner">
+      <p style="color: #000000">
+        O site utiliza cookies. Eles permitem reconhecê-lo e de receber alertas com informação sobre a sua experiência do usuário.Continuando a ver o site, eu concordo com o uso de cookies, proprietário do site de acordo com 
+        <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie"
+          >A política de cookies</a
+        >
+      </p>
+
+      <button class="close-cookie">×</button>
+    </div>
+
+    <script>
+      window.onload = function () {
+        $('.close-cookie').click(function () {
+          $('.cookie-banner').fadeOut();
+        });
+      };
+    </script>
+
+    <script>
+      let elems = document.querySelectorAll('.server-name');
+      elems.forEach((elem) => {
+        elem.innerHTML = window.location.hostname;
+      });
+    </script>
+
+    <!-- ALL JS FILES -->
+    <script src="js/all.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/custom.js"></script>
+  </body>
+</html>
